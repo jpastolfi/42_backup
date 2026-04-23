2af936f363f359dcb014c635a7edf2c069d73c1b
Joao Astolfi <jpastolfi@gmail.com>
2026-04-23 09:50:44 +0100

    lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -11,3 +11,37 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+t_list *ft_lstlast(t_list *lst)
+{
+  while (lst->next)
+  {
+    lst = lst->next;
+  }
+  return (lst);
+}
+
+int main(void)
+{
+  t_list *first = ft_lstnew("first");
+  t_list *second = ft_lstnew("second");
+  t_list *third = ft_lstnew("third");
+  t_list *fourth = ft_lstnew("fourth");
+  first->next = second;
+  second->next = third;
+  third->next = fourth;
+  fourth->next = NULL;
+  printf("first->content: %s\n", (char *)first->content);
+  printf("first->next: %p\n", first->next);
+  printf("second address: %p\n", second);
+  printf("second->content: %s\n", (char *)second->content);
+  printf("second->next: %p\n", second->next);
+  printf("third address: %p\n", third);
+  printf("third->content: %s\n", (char *)third->content);
+  printf("third->next: %p\n", third->next);
+  printf("fourth address: %p\n", fourth);
+  printf("fourth->content: %s\n", (char *)fourth->content);
+  printf("fourth->next: %p\n", fourth->next);
+  printf("Last item's content: %s\n", (char *)ft_lstlast(first)->content);
+}
